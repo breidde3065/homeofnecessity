f6d98c037a20b89e7e1e9fac591afe83c2025153
breidde3065 <breidde3065@mailfence.com>
2025-06-02 12:37:47 +0300

    Update serve.ts

--- api/serve.ts
+++ api/serve.ts
@@ -1,4 +1,24 @@
 import {VercelRequest, VercelResponse} from '@Vercel/node';
 export default function handler(req: VercelRequest, res: VercelResponse){
-  res.status(200).json({message: 'Hello from vercel API!'});
+if(req.method !== 'POST'){
+  return res.status(405).json({message:'only POST requests are allowed'})
+}
+
+  const {customerName, productID, quantity}=req.body
+
+  if(!customerName || !productID|| !quantity){
+    return res.status(400).json({message:'Missing required fields'})
+  }
+  const fakeOrderID= Math.floor(Math.random() *1000000)
+
+  return res.status(200).json({
+    message:'Order created successfully',
+    order:{
+      id: fakeOrderID,
+      customerName,
+      productID,
+      quantity
+    }
+  })
+  
 }
